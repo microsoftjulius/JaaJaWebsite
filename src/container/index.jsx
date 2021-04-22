3fef6082f3b7211598b5bb1d5f5c88dc8ff3c9ea
microsoftjulius <julisema4@gmail.com>
2021-04-23 01:12:01 +0700

    Added the default page

--- src/container/index.jsx
+++ src/container/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+import {theme} from '../theme'
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20%;
+    font-size: 30px;
+    color: ${theme.primary}
+`
+export function SectionOne(){
+    return <Container>
+            <div>Welcome to Jaaja Website Development</div>
+        </Container>
 
-export function fixedNavBar(props){
-    return <div>Welcome to Jaaja Website Development</div>
+    
 }
